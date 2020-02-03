b81d05a86e48bbd163a99d654aee05af4a4fcee8
Gabriela de Lima Dias <Gabriela.Dias@salas.aulas>
2020-02-03 20:11:15 -0300

    Update Terraform main.tf

--- main.tf
+++ main.tf
@@ -36,12 +36,6 @@ resource "google_compute_instance" "helloworld" {
     command = "echo ${google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip} > ansible/inventory"
   }
 
-
-  # Executa a playbook na máquina provisionada
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ansible/inventory --private-key ../id_rsa ansible/playbook.yml"
-  }
-
 }
 
 ## Cria SQL Gerenciado com Engine MySQL 
